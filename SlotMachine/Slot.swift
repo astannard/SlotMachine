acda388540cc7ab9400c3d4ad1c5142c955babee
Andy <stannarda@gmail.com>
2015-01-11 21:01:05 +0000

    setup slots

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,17 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Andy on 11/01/2015.
+//  Copyright (c) 2015 Andy Stannard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+    
+}
